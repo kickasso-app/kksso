3eff6ea6200fb53edc6db03436f1a3bab3bd2679
kksso <kickasso.app@gmail.com>
2023-08-16 22:28:30 +0200

    update join

--- pages/join/index.js
+++ pages/join/index.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 import { useAuth } from "services/auth";
+
+import { Row, Col } from "react-flexbox-grid/dist/react-flexbox-grid";
+
 import {
   Box,
   FormField,
@@ -10,14 +13,17 @@ import {
   Anchor,
   Paragraph,
   Notification,
-  // Button as GrommetButton,
 } from "grommet";
 import Button from "components/Button";
 
 export default function Join() {
+
   const [loading, setLoading] = useState(false);
   const [newUser, setNewUser] = useState(true);
+  // const [forgotPassword, setForgotPassword] = useState(false);
+
   const [signUpToast, setSignUpToast] = useState(false);
+  // const [magicLinkToastToast, setMagicLinkToast] = useState(false);
 
   const router = useRouter();
 
@@ -62,10 +68,10 @@ export default function Join() {
       setLoading(true);
       const { error } = await signIn({ email, password });
       if (error) throw error;
-      if (user) {
-        router.push("/");
-        // router.push("/welcome");
-      }
+      // if (user) {
+      //   router.push("/");
+      //   // router.push("/welcome");
+      // }
     } catch (error) {
       alert(error.error_description || error.message);
     } finally {
@@ -73,70 +79,128 @@ export default function Join() {
     }
   };
 
+  // const handleMagicLink = async (event) => {
+  //   event.preventDefault();
+  //   const email = event.target.email.value;
+
+  //   try {
+  //     setLoading(true);
+  //     const { error } = await sendMagicLink({ email });
+  //     if (error) throw error;
+  //   } catch (error) {
+  //     alert(error.error_description || error.message);
+  //   } finally {
+  //     setLoading(false);
+  //     setMagicLinkToast(true);
+  //   }
+  // };
+
+
   const fieldMargin = { vertical: "medium" };
   const textMargin = { bottom: "medium" };
 
   return (
-    <>
-      <form onSubmit={newUser ? handleSignUp : handleLogIn}>
-        <Box width="large" pad="medium">
-          <Heading level={3}>{newUser ? "Sign Up" : "Login"}</Heading>
-          <Box>
-            <FormField name="email" label="Email" margin={fieldMargin} required>
-              <TextInput
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                placeholder="Your email"
-              />
-            </FormField>
-          </Box>
-          <Box>
-            <FormField
-              name="password"
-              label="Password"
-              margin={fieldMargin}
-              required
-            >
-              <TextInput type="password" id="password" name="password" />
-            </FormField>
-          </Box>
-          <Box margin={fieldMargin}>
-            <Button btnStyle="filled" type="submit">
-              {/* Still something is wrong with grommet button styles
+
+    <Row around="xs">
+      <Col xs={12} md={8}>
+        <form onSubmit={newUser ? handleSignUp : handleLogIn}>
+          <Box pad="medium" >
+            <Heading level={3} alignSelf="center">{newUser ? "Sign Up as an Artist" : "Login"}</Heading>
+            <Box >
+              <FormField name="email" label="Email" margin={fieldMargin} required>
+                <TextInput
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  placeholder="Your email"
+                />
+              </FormField>
+            </Box>
+            <Box>
+              <FormField
+                name="password"
+                label="Password"
+                margin={fieldMargin}
+                required
+              >
+                <TextInput type="password" id="password" name="password" />
+              </FormField>
+
+            </Box>
+            <Box margin={fieldMargin}>
+              <Button btnStyle="filled" type="submit">
+                {/* Still something is wrong with grommet button styles
                can't pad or size correctly */}
-              {/* <GrommetButton primary size="large" type="submit"> */}
-              {loading ? "Loading..." : newUser ? "Sign Up" : "Login"}
-              {/* </GrommetButton> */}
-            </Button>
-          </Box>
+                {/* <GrommetButton primary size="large" type="submit"> */}
+                {loading ? "Loading..." : newUser ? "Sign Up" : "Login"}
+                {/* </GrommetButton> */}
+              </Button>
+            </Box>
 
-          <br />
+            <br />
 
-          {newUser ? (
-            <Paragraph>
-              Already have an account?{" "}
-              <Anchor onClick={() => setNewUser(false)}>Log In</Anchor>
-            </Paragraph>
-          ) : (
-            <Paragraph>
-              Don't have an account?{" "}
-              <Anchor onClick={() => setNewUser(true)}>Sign Up</Anchor>
-            </Paragraph>
-          )}
+            {newUser ? (
+              <>
+                <Paragraph>
+                  Already have an account?{" "}
+                  <Anchor onClick={() => setNewUser(false)}>Log In</Anchor>
+                </Paragraph>
+              </>
 
-          {signUpToast && (
+            ) : (
+              <>
+                <Paragraph>
+                  Don't have an account?{" "}
+                  <Anchor
+                    onClick={() => {
+                      setNewUser(true)
+                      setForgotPassword(false);
+                    }}>
+                    Sign Up First</Anchor>
+                  <br /> <br />
+                </Paragraph>
+
+                {/* {!forgotPassword && */}
+                <Paragraph>
+
+                  Forgot your passoword?
+                  <br />
+                  Please let us know by email and we will send you a magic link.
+                  {/* <Anchor onClick={() => setForgotPassword(true)}>Send me a magic link</Anchor> */}
+                </Paragraph>
+                {/* } */}
+              </>
+
+            )}
+
+
+
+            {signUpToast && (
+              <Notification
+                toast
+                status="normal"
+                title="Almost there!"
+                message="Please check your inbox to verify your email."
+                onClose={() => { setSignUpToast(false) }}
+              />
+            )}
+
+            {/* {magicLinkToastToast && (
             <Notification
               toast
               status="normal"
-              title="Almost there!"
-              message="Please check your inbox to verify your email."
+              title="We sent you a magic link!"
+              message="Please check your inbox to log in to your account."
               onClose={() => { setSignUpToast(false) }}
             />
-          )}
-        </Box>
-      </form>
-    </>
+          )} */}
+
+          </Box>
+        </form>
+      </Col>
+    </Row>
+
+
   );
 }
